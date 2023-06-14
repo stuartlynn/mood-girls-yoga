06232868dab574721676e34c1c80db9387feed1f
Stuart Lynn <stuart.lynn@gmail.com>
2023-06-14 17:39:14 +0100

    overrides for styled on section headers

--- nextjs-mood-girls-yoga/components/section-header.js
+++ nextjs-mood-girls-yoga/components/section-header.js
@@ -1,3 +1,3 @@
-export default function SectionHeader({ children }) {
-    return <h2 className="text-center md:text-left text-4xl">{children}</h2>;
+export default function SectionHeader({ children, className }) {
+    return <h2 className={`${className} text-center md:text-left text-4xl`}>{children}</h2>;
 }
